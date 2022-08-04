489512d2206f8914912a7e6491bb202b3755b39d
synodriver <diguohuangjiajinweijun@gmail.com>
2022-08-04 17:12:34 +0800

    add build script

--- docker-images/python3.10-alpine3.16.dockerfile
+++ docker-images/python3.10-alpine3.16.dockerfile
@@ -1,4 +1,4 @@
-FROM python:3.10.6-alpine
+FROM python:3.10-alpine3.16
 
 LABEL maintainer="synodriver"
 
---
+++ scripts/build_push.py
@@ -0,0 +1,37 @@
+"""
+Copyright (c) 2008-2022 synodriver <synodriver@gmail.com>
+"""
+import asyncio
+import os
+
+NAME = os.getenv("NAME")
+
+
+async def build_one():
+    p = await asyncio.create_subprocess_exec(
+        "docker",
+        "login",
+        "-u",
+        os.getenv("DOCKER_USERNAME"),
+        "-p",
+        os.getenv("DOCKER_PASSWORD"),
+    )
+    await p.wait()
+    p = await asyncio.create_subprocess_exec(
+        "docker",
+        "build",
+        "-f",
+        f"./docker-images/{NAME}.dockerfile",
+        "-t",
+        f"synodriver/nonecorn-gunicorn-docker:{NAME}",
+        "./docker-images/",
+    )
+    await p.wait()
+    p = await asyncio.create_subprocess_exec(
+        "docker", "push", f"synodriver/nonecorn-gunicorn-docker:{NAME}"
+    )
+    await p.wait()
+
+
+if __name__ == "__main__":
+    asyncio.run(build_one())
